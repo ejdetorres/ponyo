f8c0873f0e8f5bde2cd42c798d506bfa5c534521
ejdetorres <160102977+ejdetorres@users.noreply.github.com>
2026-08-02 18:27:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -187,18 +187,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const noBtn = document.getElementById("noBtn");
 
-const funnyText = document.getElementById("funnyText");
-
-const messages = [
-    "Wait... 🥺",
-    "Think again 😂",
-    "You're too fast! 😭",
-    "Almost caught me ❤️",
-    "Okay... I'll stop running 😊"
-];
-
-let messageIndex = 0;
-
 noBtn.addEventListener("mouseenter", moveButton);
 
 function moveButton() {
@@ -213,11 +201,5 @@ function moveButton() {
 
     noBtn.style.left = x + "px";
     noBtn.style.top = y + "px";
-
-    if (funnyText) {
-        funnyText.textContent = messages[messageIndex];
-        messageIndex = (messageIndex + 1) % messages.length;
-
-}
 
 }
