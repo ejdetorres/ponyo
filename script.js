85e22cde1b772fe0b048365fa5c8d86bfbf87d8c
ejdetorres <160102977+ejdetorres@users.noreply.github.com>
2026-08-02 15:45:09 +0800

    Update script.js

--- script.js
+++ script.js
@@ -86,6 +86,7 @@ document.addEventListener("DOMContentLoaded", () => {
     ========================================== */
 
     const yesBtn = document.getElementById("yesBtn");
+    const noBtn = document.getElementById("noBtn");
 
     if(yesBtn){
 
@@ -184,3 +185,18 @@ document.addEventListener("DOMContentLoaded", () => {
     };
 
 });
+
+function moveButton() {
+
+    const area = document.querySelector(".button-area");
+
+    const maxX = area.clientWidth - noBtn.offsetWidth;
+    const maxY = area.clientHeight - noBtn.offsetHeight;
+
+    const x = Math.random() * maxX;
+    const y = Math.random() * maxY;
+
+    noBtn.style.left = x + "px";
+    noBtn.style.top = y + "px";
+
+}
